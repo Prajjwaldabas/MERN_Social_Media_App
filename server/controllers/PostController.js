ca0c8638323647e953a6a167e4d68bbdd8c90961
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-10-23 00:13:57 +0530

    new commit

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -48,7 +48,7 @@ export const updatePost = async (req, res) => {
 export const deletePost = async (req, res) => {
  
   const id = req.params.id;
-  const { userId } = req.body;
+  const userId = req.query.userId;
 
   try {
   
